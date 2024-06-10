a58f0c864eb4f8dcca947c169a43a45b5928247a
iTsThermo <117327847+iTsThermo@users.noreply.github.com.>
2024-06-10 14:36:08 -0400

    Fixed issue with innerHTML (removed it and replaced it with display styling). Changed the csv file format to match the convertion data types/name variable. Added instructions to read me file with link to JSON to CSV converstion website. Added comments for better understandability and cleaned up code.

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ flask site implementing sonification
 
 How to use:
 1. Read requirements.txt file
-2. Run app.py file
+2. Run app.py with preloaded data files (Requires 1 csv file to be named "Default")
 
+For convertions from JSON to CSV:
+Use https://data.page/json/csv for best results
--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt  # Importing matplotlib for plotting
 import pandas as pd  # Importing pandas for data manipulation
-from flask import Flask, render_template, request, redirect  # Importing Flask for web app and render_template for rendering HTML templates
+from flask import Flask, render_template, request # Importing Flask for web app and render_template for rendering HTML templates
 from plotnine import *  # Importing plotnine for ggplot functionality
-from plotnine import theme  # Importing theme module from plotnine for customizing plots
 import os
 import datetime as dt
 import re
@@ -14,8 +13,10 @@ import plotly.express as px  # Importing plotly.express for creating interactive
 # Initializing the Flask application
 app = Flask(__name__, static_url_path='/static')
 
-baseFolderPath = './data'
+# Base folder where the program will find the csv files
+base_folder_path = './data'
 
+# formats the dates to only have Year, month, and date (hours, minutes and seconds are excluded)
 def getTimeStampString(tsec: float) -> str:
     time_obj = dt.datetime.fromtimestamp(tsec)
     time_str = dt.datetime.strftime(time_obj, '%Y-%m-%d')
@@ -24,18 +25,17 @@ def getTimeStampString(tsec: float) -> str:
 
 @app.route('/', methods=["GET","POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == 'POST' or request.method == "GET":
         selected_file = request.form.get('name')
-        new_file = './data/' + str(selected_file) + '.csv'
+        if str(selected_file) == "None":
+            selected_file = "Default"
+        new_file = './data/' + selected_file + '.csv'
         df = pd.read_csv(new_file)
-    else:
-    # Reading the CSV file and loading the data into a pandas dataframe
-        df = pd.read_csv('./data/data.csv')
 
     # Melting the dataframe to a long format for easier plotting using pandas
     df_melted = df.melt(
         id_vars=['timestamp'],
-        value_vars=['Conductivity', 'Dissolved Oxygen', 'Salinity', 'Temperature(F)', 'Turbidity', 'pH'],
+        value_vars=['sensors__Cond', 'sensors__DOpct', 'sensors__Sal', 'sensors__Temp', 'sensors__Turb', 'sensors__pH'],
         var_name='Variable',
         value_name='Value'
     )
@@ -44,24 +44,24 @@ def index():
     df_melted['Original_Value'] = df_melted['Value']
 
     # Scaling the values for each variable to make them comparable in the stacked area plot using pandas
-    df_melted.loc[df_melted['Variable'] == 'Salinity', 'Value'] *= 30
-    df_melted.loc[df_melted['Variable'] == 'Dissolved Oxygen', 'Value'] *= 1
-    df_melted.loc[df_melted['Variable'] == 'Temperature(F)', 'Value'] *= 1
-    df_melted.loc[df_melted['Variable'] == 'Conductivity', 'Value'] *= 0.3
-    df_melted.loc[df_melted['Variable'] == 'pH', 'Value'] *= 10
-    df_melted.loc[df_melted['Variable'] == 'Turbidity', 'Value'] *= 1
+    df_melted.loc[df_melted['Variable'] == 'sensors__Sal', 'Value'] *= 30
+    df_melted.loc[df_melted['Variable'] == 'sensors__DOpct', 'Value'] *= 1
+    df_melted.loc[df_melted['Variable'] == 'sensors__Temp', 'Value'] *= 1
+    df_melted.loc[df_melted['Variable'] == 'sensors__Cond', 'Value'] *= 0.3
+    df_melted.loc[df_melted['Variable'] == 'sensors__pH', 'Value'] *= 10
+    df_melted.loc[df_melted['Variable'] == 'sensors__Turb', 'Value'] *= 1
 
     # Defining the desired order of the variables for the legend and plotting
-    desired_order = ['Salinity', 'pH', 'Temperature(F)', 'Turbidity', 'Dissolved Oxygen', 'Conductivity']
+    desired_order = ['sensors__Sal', 'sensors__pH', 'sensors__Temp', 'sensors__Turb', 'sensors__DOpct', 'sensors__Cond']
 
     # Defining custom colors for each variable
     color_dict = {
-        'Salinity': '#FF9F1C',
-        'Dissolved Oxygen': '#8FE1F4',
-        'Temperature(F)': '#169873',
-        'Conductivity': '#2081C3',
-        'pH': '#EFE9AE',
-        'Turbidity': '#B57BA6'
+        'sensors__Sal': '#FF9F1C',
+        'sensors__DOpct': '#8FE1F4',
+        'sensors__Temp': '#169873',
+        'sensors__Cond': '#2081C3',
+        'sensors__pH': '#EFE9AE',
+        'sensors__Turb': '#B57BA6'
     }
 
     # Creating a stacked area plot using Plotly Express
@@ -87,15 +87,16 @@ def index():
         font_color="white",  # Setting main font color
         title_font_family="Roboto",  # Setting title font family
         legend_tracegroupgap=0,  # Setting gap between legend items
-    title={
-        'y':.96,
-        'x':.46,
-        'xanchor': 'center'},
+        title={'y':.96, 'x':.46, 'xanchor': 'center'},
         margin_b=10,  # Setting bottom margin
         margin_l=25,  # Setting left margin
         margin_r=25,  # Setting right margin
         margin_t=40  # Setting top margin
     )
+
+    #Changes the names of the imported data set to be more readable 
+    newnames = {'sensors__Cond' : 'Conductivity', 'sensors__DOpct' : 'Dissolved Oxygen', 'sensors__Sal' : 'Salinity', 'sensors__Temp' : 'Temperature(F)', 'sensors__Turb' : 'Turbidity', 'sensors__pH' : 'pH'}
+    fig.for_each_trace(lambda t: t.update(name = newnames[t.name], legendgroup = newnames[t.name], hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
 
     #fig.legend(bbox_to_anchor=(1.1, 1.05))
     #Extract data parameters
@@ -107,21 +108,27 @@ def index():
     # Printing the figure to the console for debugging purposes
     print(fig)
 
-    pattern = r"\\(.+)"
-    def file_object(inputed_file):
-        file = inputed_file.stat()
-        file_date = getTimeStampString(file.st_mtime)
-        name_only = os.path.splitext(inputed_file)[0]
-        match = re.search(pattern, name_only)
-        return {'name': match.group(1), 'date': file_date}
-    file_names = [file_object(x) for x in os.scandir(baseFolderPath)] 
+    file_names = [file_object(x) for x in os.scandir(base_folder_path)] 
 
     # Converting the plot to HTML format to embed in the web page using Plotly
     plot_html = fig.to_html(include_plotlyjs='cdn')   
     
     # Rendering the graph.html template and passing the plot HTML to it using Flask
     return render_template('graph.html', plot=plot_html, files=file_names)
-    
+
+# Searches the data folder for file names and imports them
+# Return a object of the file name only (no extension ".csv")
+pattern = r"\\(.+)"
+def file_object(inputed_file):
+    # import date of files
+    # file = inputed_file.stat()
+    # file_date = getTimeStampString(file.st_mtime)
+    name_only = os.path.splitext(inputed_file)[0]
+    match = re.search(pattern, name_only)
+    # import date of file in the object
+    # return {'name': match.group(1), 'date': file_date}
+    return {'name': match.group(1)}
+
 
 # Running the Flask application
 if __name__ == '__main__':
